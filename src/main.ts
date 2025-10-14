4fc414eb87121eaa74845c4973c5f94f99780e3c
Mario Henrique <mariohenriquelp@gmail.com>
2025-10-14 19:33:28 -0300

    feat: instalando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+    const config = new DocumentBuilder()
+        .setTitle("Prime Victory")
+        .setDescription("Documentation for PV API")
+        .setVersion("0.1")
+        .addTag("PV")
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
+    await app.listen(3000, "0.0.0.0");
 }
 bootstrap();
